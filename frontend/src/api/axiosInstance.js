143ff59264adcd114af4e44ec6b1472a0978b5ac
Avinash829 <avinashpappala@gmail.com>
2026-08-03 19:13:41 +0530

    verification improved

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -11,7 +11,7 @@ import { STORAGE_KEYS, ERROR_MESSAGES } from "../constants";
 
 const axiosInstance = axios.create({
     baseURL: config.api.baseUrl,
-    timeout: 120000,
+    timeout: 240000,
     headers: {
         "Content-Type": "application/json",
     },
